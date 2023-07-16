51e0d8f4868c5a1e5003a073dd20f20fcf8a7992
wwonnn <dnjsgml0508@naver.com>
2023-07-16 13:06:10 +0900

    home

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Vector2 inputVec;
+    public float speed;
+
+    Rigidbody2D rigid;
+    SpriteRenderer spriter;
+    Animator anim;
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        spriter = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //캐릭터 움직임
+        inputVec.x = Input.GetAxisRaw("Horizontal");
+        //inputVec.y = Input.GetAxisRaw("Vertical");
+
+        //애니메이션 구현
+/*        if (inputVec.x > 0)         //오른쪽 달리기
+        {
+            spriter.flipX = false;
+            anim.SetBool("isRun", true);
+            anim.SetBool("isRunLeft", false);
+        }
+        else if (inputVec.x < 0)    //왼쪽 달리기
+        {
+            spriter.flipX = true;
+            //anim.SetBool("isRun", false);
+            //anim.SetBool("isRunLeft", true);
+        }
+        else                        //Idle
+        {
+            anim.SetBool("isRun", false);
+            anim.SetBool("isRunLeft", false);
+        }*/
+
+        if(inputVec.x != 0)
+        {
+            spriter.flipX = inputVec.x < 0;
+            anim.SetBool("isRun", true);
+            anim.SetBool("isRunLeft", false);
+        }
+        else                        //Idle
+        {
+            anim.SetBool("isRun", false);
+            anim.SetBool("isRunLeft", false);
+        }
+
+    }
+
+    void FixedUpdate()
+    {
+        //캐릭터 움직임
+        if (rigid.position.x >= 9)
+        {
+            rigid.position = new Vector2(9, transform.position.y);
+
+        }
+        else if (rigid.position.x <= -9)
+        {
+            rigid.position = new Vector2(-9, transform.position.y);
+        }
+
+        Vector2 nextVec = inputVec.normalized * speed * Time.fixedDeltaTime;
+        rigid.MovePosition(rigid.position + nextVec);
+
+    }
+}
