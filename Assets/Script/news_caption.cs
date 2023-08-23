7505d1ca3a857af4615cb8e2121cd5ed21162e8c
hayong39 <yia0524_hy@naver.com>
2023-08-23 12:53:19 +0900

    뉴스 자막 추가

---
+++ Assets/Script/news_caption.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using UnityEngine;
+
+public class news_caption : MonoBehaviour
+{
+    public GameObject firstObject;
+    public GameObject secondObject;
+
+    private void Start()
+    {
+        secondObject.SetActive(false); 
+       StartCoroutine(ShowFirstObjectAndDisappear());
+        
+    }
+
+    IEnumerator ShowFirstObjectAndDisappear()
+    {
+        firstObject.SetActive(true); // 첫 번째 오브젝트를 활성화하여 나타나게 합니다.
+        yield return new WaitForSeconds(4f); // 4초 동안 대기
+
+        firstObject.SetActive(false);
+        secondObject.SetActive(true);// 첫 번째 오브젝트를 비활성화하여 사라지게 합니다.
+    }
+
+    /*IEnumerator ShowSecondObjectAfterDelay()
+    {
+        yield return new WaitForSeconds(4f); // 4초 지연
+
+        secondObject.SetActive(true); // 두 번째 오브젝트를 활성화하여 나타나게 합니다.
+    }*/
+}
