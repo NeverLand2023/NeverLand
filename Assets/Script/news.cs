de26f98fc3653d32400d2e0ac4abb064a3fe3855
yooksj13 <yooksj13@ewhain.net>
2023-08-24 17:12:24 +0900

    fix canvas

--- Assets/Script/news.cs
+++ Assets/Script/news.cs
@@ -1,13 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class news : MonoBehaviour
 {
+    public GameObject news1;
+    public GameObject news2;
     // Start is called before the first frame update
     void Start()
     {
+        Invoke("EnableNews2", 4.5f);
         Invoke("sceneChange", 9);
     }
 
@@ -19,5 +23,10 @@ public class news : MonoBehaviour
     void sceneChange() 
     {
         SceneManager.LoadScene("home");
+    }
+    private void EnableNews2()
+    {
+        news1.SetActive(false);
+        news2.SetActive(true);
     }
 }
