5a30029d7b3b993685c6869dfb3f1e853054c7a7
wwonnn <dnjsgml0508@naver.com>
2023-12-24 15:37:54 +0900

    attack monster
    effect에 맞으면 monter 공격 받게 수정

--- Assets/Script/AttackEffect.cs
+++ Assets/Script/AttackEffect.cs
@@ -4,16 +4,26 @@ using UnityEngine;
 
 public class AttackEffect : MonoBehaviour
 {
+    public GameObject hook;
+    Transform transform;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform = GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (hook.GetComponent<SpriteRenderer>().flipX)
+        {
+            transform.localPosition = new Vector3(-0.35f, 0, 0);
+        }
+        else
+        {
+            transform.localPosition = new Vector3(0.35f, 0, 0);
+        }
     }
     void AttackEffectEnd()
     {
