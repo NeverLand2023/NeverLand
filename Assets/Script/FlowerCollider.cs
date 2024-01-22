a0119b141f322f8a452e5bd5c7ac19f942fc5837
yoshii <113421649+yooksj13@users.noreply.github.com>
2024-01-23 02:01:05 +0900

    merge scenes

--- Assets/Script/FlowerCollider.cs
+++ Assets/Script/FlowerCollider.cs
@@ -5,9 +5,25 @@ using UnityEngine;
 public class FlowerCollider : MonoBehaviour
 {
     public GameObject FlowerWall;
+    private bool HookEnter = false;
+
+    private void Update()
+    {
+        if (HookEnter && Input.GetKeyDown(KeyCode.Space))
+        {
+            FlowerWall.SetActive(false);
+
+        }
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        FlowerWall.SetActive(false);
+        HookEnter = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+
+        HookEnter = false;
     }
 }
