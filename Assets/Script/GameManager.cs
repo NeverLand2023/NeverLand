271f093db83c95a5245470048206cada6a2938bc
wwonnn <dnjsgml0508@naver.com>
2024-01-23 06:55:04 +0900

    lighter inventory

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -128,6 +128,10 @@ public class GameManager : MonoBehaviour
             {
                 Inventory.InventorySend(invenObjects[1], clickObjects[3], "DiaryNote0");
             }
+            else if (itemName == "Lighter")
+            {
+                Inventory.InventorySend(invenObjects[2], null, "Lighter");
+            }
         }
     }
 
---
+++ Assets/Script/Lighter.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lighter : MonoBehaviour
+{
+    public GameObject lighter;
+    public GameObject lighter_ui;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Inventory.InventorySend(lighter_ui, null, "Lighter");
+                lighter.SetActive(false);
+            }
+        }
+    }
+}
