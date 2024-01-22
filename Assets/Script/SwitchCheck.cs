587ce3335b9d3b0dc94297b50ea1729c05a443fa
HyejinYoo <104633224+HyejinYoo@users.noreply.github.com>
2024-01-23 00:15:22 +0900

    스위치

---
+++ Assets/Script/SwitchCheck.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchCheck : MonoBehaviour
+{
+    public GameObject PointLight;
+    public GameObject[] switchButtons = new GameObject[4];
+    private SwitchButton[] btn = new SwitchButton[4];
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        btn[0] = switchButtons[0].GetComponent<SwitchButton>();
+        btn[1] = switchButtons[1].GetComponent<SwitchButton>();
+        btn[2] = switchButtons[2].GetComponent<SwitchButton>();
+        btn[3] = switchButtons[3].GetComponent<SwitchButton>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SwitchUpdate();
+    }
+
+    public void SwitchUpdate()
+    {
+      
+        if (!btn[0].isUpState && btn[1].isUpState && btn[2].isUpState && btn[3].isUpState)
+        {
+            Main_Hook.attackAvailable = true;
+            Destroy(PointLight);
+            Destroy(gameObject);
+        }
+    }
+}
