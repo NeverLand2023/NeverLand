ffd6e3698c77d7263b0676d1d6112a0640f3a694
HyejinYoo <104633224+HyejinYoo@users.noreply.github.com>
2024-01-23 04:45:41 +0900

    stair_lock

---
+++ Assets/Script/StairLock.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class StairLock : MonoBehaviour
+{
+
+    private bool colState = false;
+    private bool hasKey = false;
+
+    private void Update()
+    {
+        // 스페이스바를 누르면
+        if (colState && Input.GetKeyDown(KeyCode.Space))
+        {
+            //키 있는지 없는지 확인
+            //has key = 
+            if (hasKey)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // 충돌이 발생하면
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            colState = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        // 충돌이 발생하면
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            colState = false;
+        }
+    }
+}
