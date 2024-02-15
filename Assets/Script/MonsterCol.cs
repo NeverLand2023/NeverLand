81f135eb3726f3c5fc112e666dcc2c18a54b75d8
HyejinYoo <104633224+HyejinYoo@users.noreply.github.com>
2024-02-15 18:08:16 +0900

    monster

---
+++ Assets/Script/MonsterCol.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class MonsterCol : MonoBehaviour
+{
+    public GameObject readyObject;
+    public GameObject monsterObject;
+    private bool active = false;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!active && other.CompareTag("Player"))
+        {
+            Destroy(readyObject);
+            monsterObject.SetActive(true);
+            active = true;
+        }
+    }
+}
