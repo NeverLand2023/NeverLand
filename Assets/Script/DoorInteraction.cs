321fdcf6c0140dc705617bb94d15c0e2d2e46ebd
wwonnn <dnjsgml0508@naver.com>
2024-02-19 13:54:04 +0900

    0219 box

--- Assets/Script/DoorInteraction.cs
+++ Assets/Script/DoorInteraction.cs
@@ -5,28 +5,41 @@ using UnityEngine;
 public class DoorInteraction : MonoBehaviour
 {
     public Transform arriveTrans;
+    public bool BoxButtIn = false;
+    public bool BoxButtOut = false;
 
-    private bool hookEnter= false;
+    private bool hookEnter = false;
     private GameObject Hook;
+    private GameObject ResetButton;
 
     private void Start()
     {
+        ResetButton = GameObject.Find("ResetButton");
         Hook = GameObject.FindWithTag("Player");
     }
 
     private void Update()
     {
-        if(hookEnter && Input.GetKeyDown(KeyCode.Space))
+        if (hookEnter && Input.GetKeyDown(KeyCode.Space))
         {
             Hook.transform.position = arriveTrans.position;
             Debug.Log("going from " + this.gameObject.name);
             SoundManager.instance.doorSound.Play();
+            if (BoxButtIn)
+            {
+                ResetButton.SetActive(true);
+            }
+            else if (BoxButtOut)
+            {
+
+                ResetButton.SetActive(false);
+            }
         }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.tag == "Player")
+        if (collision.tag == "Player")
         {
             hookEnter = true;
 
